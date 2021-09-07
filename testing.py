65f8aa4b34e2e42f23bacad2ad1a142e5e1d6b1f
alexfozkos <afozkos@alaska.edu>
2021-09-07 12:07:41 -0800

    Updating project 9/7

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
+import matplotlib.colors
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import matplotlib as mpl
 import xml.etree.ElementTree as ET
-
+from matplotlib.colors import LinearSegmentedColormap
 
 tree = ET.parse('grid.xml')
 root = tree.getroot()
@@ -15,13 +15,26 @@ nlon = int(root[1].attrib['nlon'])
 gridDataArray = np.array(root[-1].text.replace('\n', ' ').split(' ')[1:-1], dtype=float)
 
 gridDataArray = np.reshape(gridDataArray, (int(nlat*nlon), 9))
-print(gridDataArray[:, 1])
+# print(gridDataArray[:, 1])
 
 xlon = gridDataArray[:, 0]
 ylat = gridDataArray[:, 1]
 pgaScale = gridDataArray[:, 3]
+print(np.max(pgaScale))
 
-viridis = cm.get_cmap('viridis', 256)
+shakemap_colors = [(1, 1, 1),  # white
+                   (212/255, 201/255, 245/255),  # lavender
+                   (97/255, 255/255, 230/255),  # light blue
+                   # (215/255, 234/255, 92/255),  # yellow-green
+                   (1, 239/255, 0),  # yellow
+                   # (1, 168, 0),  # orange
+                   (184/255, 0, 0),  # red
+                   (101/255, 0, 0)  # dark red
+                   ]
+my_cmap = LinearSegmentedColormap.from_list('shakemap_colors', shakemap_colors)
 
-plt.scatter(xlon, ylat, c = pgaScale)
+plt.scatter(xlon, ylat, s=.3, c=pgaScale, cmap=my_cmap, norm=matplotlib.colors.LogNorm())
+cbar = plt.colorbar(label='PGA (%g)', extend='max', ticks=[0, 0.0464, 0.297, 2.76, 6.2, 11.5, 21.5, 40.1, 74.7, 139])
+cbar.set_ticklabels(['0', '0.0464', '0.297', '2.76', '6.2', '11.5', '21.5', '40.1', '74.7', '139'])
+# plt.clim(0.001, 139)
 plt.show()
