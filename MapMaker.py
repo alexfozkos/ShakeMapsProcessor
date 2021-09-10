4082e2dc827f10fd6839023c4e831a78349dffd2
alexfozkos <afozkos@alaska.edu>
2021-09-09 18:09:30 -0800

    More work with PyGMTMapMaker.py, working on converting matplotlib concepts into pygmt concepts. Using haversine to calculate rectangular area around a point that it covers for nice graphical representation.

--- MapMaker.py
+++ MapMaker.py
@@ -3,7 +3,7 @@ import matplotlib.colors
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 import xml.etree.ElementTree as ET
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm, ListedColormap
 
 tree = ET.parse('grid.xml')
 root = tree.getroot()
@@ -19,25 +19,29 @@ gridDataArray = np.reshape(gridDataArray, (int(nlat*nlon), 9))
 
 xlon = gridDataArray[:, 0]
 ylat = gridDataArray[:, 1]
-pgaScale = gridDataArray[:, 3]
-print(np.max(pgaScale))
+pga = gridDataArray[:, 3]
+# print(np.max(pgaScale))
+cbounds = np.array([[0, 0.0464, 0.297, 2.76, 6.2, 11.5, 21.5, 40.1, 74.7, 139]])
 
 shakemap_colors = [(1, 1, 1),  # white
-                   (212/255, 201/255, 245/255),  # lavender
-                   (97/255, 255/255, 230/255),  # light blue
-                   # (215/255, 234/255, 92/255),  # yellow-green
-                   (1, 239/255, 0),  # yellow
-                   # (1, 168, 0),  # orange
-                   (184/255, 0, 0),  # red
-                   (101/255, 0, 0)  # dark red
+                   (228/255, 206/255, 235/255),  # Thistle
+                   (141/255, 246/255, 246/255),  # Electric blue
+                   (135/255, 248/255, 160/255),  # Light-green
+                   (199/255, 255/255, 110/255),  # Inchworm
+                   (255/255, 255/255, 65/255),  # Maximum yellow
+                   (255/255, 176/255, 28/255),  # Honey yellow
+                   (229/255, 78/255, 38/255),  # Flame
+                   (189/255, 35/255, 21/255),  # International orange engineering
+                   (141/255, 0/255, 0/255)  # Dark red
                    ]
-my_cmap = LinearSegmentedColormap.from_list('shakemap_colors', shakemap_colors)
+my_cmap = ListedColormap(shakemap_colors)
+norm = matplotlib.colors.BoundaryNorm(cbounds[0], len(cbounds[0])-1)  # normalizes colors to the boundaries
 
 plt.plot(elon, elat, 'k*')
-plt.scatter(xlon, ylat, s=.3, c=pgaScale, cmap=my_cmap, norm=matplotlib.colors.LogNorm())
+plt.scatter(xlon, ylat, s=.3, c=pga, cmap=my_cmap, norm=norm)
 plt.xlabel('Longitude (deg)')
 plt.ylabel('Latitude (deg)')
-cbar = plt.colorbar(label='PGA (%g)', extend='max', ticks=[0, 0.0464, 0.297, 2.76, 6.2, 11.5, 21.5, 40.1, 74.7, 139])
+cbar = plt.colorbar(label='PGA (%g)', extend='max', ticks=cbounds[0])
 cbar.set_ticklabels(['0', '0.0464', '0.297', '2.76', '6.2', '11.5', '21.5', '40.1', '74.7', '139'])
-# plt.clim(0.001, 139)
+plt.clim(0, 139)
 plt.savefig('scattermap.png')
