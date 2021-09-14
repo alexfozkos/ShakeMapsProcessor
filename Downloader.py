381cc623a50899adb9aa68866ff519ba34cb7b37
alexfozkos <afozkos@alaska.edu>
2021-09-13 17:08:37 -0800

    Played around with rectangular area for PGA, appears to be mathematically correct to me at this time (width and height are equal to figure size(15cm) divided by number of lat or lon points), but still slight overlap on edges. Is this a resolution issue? Added Epicenter and Seismic stations to the PyGMT Map. As such, Downloader.py now downloads the stations json file as well as the grid xml. I think this could be automated given one or the other from user input? Still not sure how to add hill shade. I can create an array manually with cities in the area and plot them, but I am not sure how to automate getting that data. Maybe give google api(?) lat lon bounds and grab major cities lat lon data from it? Colormaps remain arcane to me.

--- Downloader.py
+++ Downloader.py
@@ -3,8 +3,13 @@
 
 import requests
 
-url = 'https://earthquake.alaska.edu/sites/all/web/shakemap/ak0216xu2rod/grid.xml'
+url1 = 'https://earthquake.alaska.edu/sites/all/web/shakemap/ak0216xu2rod/grid.xml'
+url2 = 'https://earthquake.alaska.edu/sites/all/web/shakemap/ak0216xu2rod/stationlist.json'
 
-response = requests.get(url)
+response = requests.get(url1)
 with open('grid.xml','wb') as file:
+    file.write(response.content)
+
+response = requests.get(url2)
+with open('stations.json','wb') as file:
     file.write(response.content)
--- PyGMTMapMaker.py
+++ PyGMTMapMaker.py
@@ -1,14 +1,25 @@
 # Creates a scatter plot map of all data points in grid.xml and shades them according to PGA, using PyGMT
-import matplotlib.colors
-from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm
 
 import numpy as np
 import xml.etree.ElementTree as ET
 import pygmt
+import json
 
 # read in the xml
 tree = ET.parse('grid.xml')
 root = tree.getroot()
+
+# read in stations json
+with open('stations.json') as file:
+    stations = json.load(file)
+
+station_coords = np.array([[]])
+
+for feature in stations['features']:
+    station_coords = np.append(station_coords, feature['geometry']['coordinates'])
+
+station_coords = np.reshape(station_coords, (len(stations['features']), 2))
+
 
 # grab event coords and grid size and grid bounds
 elat = float(root[0].attrib['lat'])
@@ -30,70 +41,50 @@ gridDataArray = np.reshape(gridDataArray, (int(nlat * nlon), 9))
 lats = gridDataArray[:, 1]
 lons = gridDataArray[:, 0]
 pga = gridDataArray[:, 3]
-inc = [0.0464, 0.2506, 2.463, 3.44, 5.3, 10, 18.6, 34.6, 64.3]
-# I need to convert this matplotlib stuff to a PyGMT equivalent
-#
-# limits = np.array([[0, 0.0464, 0.297, 2.76, 6.2, 11.5, 21.5, 40.1, 74.7, 139]])
-shakemap_colors = [(1, 1, 1),  # white
-                   (228 / 255, 206 / 255, 235 / 255),  # Thistle
-                   (141 / 255, 246 / 255, 246 / 255),  # Electric blue
-                   (135 / 255, 248 / 255, 160 / 255),  # Light-green
-                   (199 / 255, 255 / 255, 110 / 255),  # Inchworm
-                   (255 / 255, 255 / 255, 65 / 255),  # Maximum yellow
-                   (255 / 255, 176 / 255, 28 / 255),  # Honey yellow
-                   (229 / 255, 78 / 255, 38 / 255),  # Flame
-                   (189 / 255, 35 / 255, 21 / 255),  # International orange engineering
-                   (141 / 255, 0 / 255, 0 / 255)  # Dark red
-                   ]
 
-
-# my_cmap = LinearSegmentedColormap.from_list('shakemap_colors', shakemap_colors)
-# norm = matplotlib.colors.BoundaryNorm(limits[0], len(limits[0])-1)
-
-
-def getDistance(lat1, lon1, lat2, lon2):
-    r = 6371  # Radius of earth in km
-    phi1 = np.radians(lat1)
-    phi2 = np.radians(lat2)
-    dphi = np.radians(lat2 - lat1)
-    dlambda = np.radians(lon2 - lon1)
-    a = (np.sin(dphi / 2)) ** 2 + np.cos(phi1) * np.cos(phi2) * (np.sin(dlambda / 2)) ** 2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
-    d = r * c  # distance in km
-    return d
-
-
-# using middle lon and lats, and max min lon and lats to calculate the average scale factor for distance per angle
-# between longitude and latitude at this location. Should give nice rectangle width/height to make as a marker
-mid_lon = np.median(gridDataArray[:, 0])
-mid_lat = np.median(gridDataArray[:, 1])
-max_lon = np.max(gridDataArray[:, 0])
-max_lat = np.max(gridDataArray[:, 1])
-min_lon = np.min(gridDataArray[:, 0])
-min_lat = np.min(gridDataArray[:, 1])
-
-k = getDistance(min_lon, mid_lat, max_lon, mid_lat)/getDistance(mid_lon, min_lat, mid_lon, max_lat)
-r_width = 0.0333
-r_height = 0.0333 * k
+r_width = 15/nlon
+r_height = 15/nlat
 
 fig = pygmt.Figure()
 fig.basemap(region=gridBoundaries, projection='M15c', frame=True)
 
+
 pygmt.makecpt(
-    transparency=10,
+    transparency=50,
     cmap=['hot'],
     reverse=True,
-    series=[0, np.max(pga)],
+    series=[0, np.max(pga)]
 )
 
-fig.plot(
+fig.plot(  # Plot seismic stations as triangles
+    x=station_coords[:, 0],
+    y=station_coords[:, 1],
+    style='t+0.1c',
+    color='white',
+    pen='black',
+)
+
+fig.plot(  # plot pga data
     x=lons,
     y=lats,
     color=pga,
     cmap=True,
     style='r{}/{}'.format(r_width, r_height)  # Use .format to insert calculated values
-    # distance(mid lon, max lat), (mid lon, min lat) = k * distance(max lon, mid lat), (min lon,
-    # mid lat) -> dist2lons = k*dist2lat, width wil be 0.0333, height will be k*0.0333
+)
+
+# fig.plot(  # plot black dots at each data point
+#     x=lons,
+#     y=lats,
+#     color='black',
+#     style='p'
+# )
+
+fig.plot(  # plot epicenter
+    x=elon,
+    y=elat,
+    style='a.2c',
+    color='purple',
+    pen='black'
 )
 
 fig.coast(shorelines=True, water='skyblue')  # draw coast over data
