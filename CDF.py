741ba3dff347b3d72352748244e576c63c64c16a
alexfozkos <afozkos@alaska.edu>
2021-10-19 23:25:29 -0800

    added 'percent of sites with atleast X warning time' for each major mmi bracket and a plot including all mmi brackets to CDF.py

--- CDF.py
+++ CDF.py
@@ -28,9 +28,34 @@ for i in points:
 # print(point_dict[5])
 #Sorts the dictionary keys by mmi, decreasing (highest mmi keys first)
 #point_dict = collections.OrderedDict(reversed(sorted(point_dict.items())))
+
+# create PDF and CDF for each major MMI in the dictionary (1, 2, 3 ...)
+# for k in point_dict.keys():
+#     # getting data of the histogram
+#     count, bins_count = np.histogram(point_dict[k], bins=10)
+#
+#     # finding the PDF of the histogram using count values
+#     pdf = count / sum(count)
+#
+#     # using numpy np.cumsum to calculate the CDF
+#     # We can also find using the PDF values by looping and adding
+#     cdf = np.cumsum(pdf)
+#
+#     # plotting PDF and CDF
+#     plt.plot(bins_count[1:], pdf, color="red", label="PDF")
+#     plt.plot(bins_count[1:], cdf, label="CDF")
+#     plt.xlabel('Warning Time (s)')
+#     plt.title('MMI {} to {}'.format(k, k + 0.9))
+#     plt.legend()
+#     plt.grid()
+#     plt.savefig('Figures/cdfs/CDF MMI {} to {}.png'.format(k, k + 0.9))
+#     plt.clf()
+
+# Plot for percent warning times at least each mmi
+plt.figure(figsize=(12, 6))
 for k in point_dict.keys():
     # getting data of the histogram
-    count, bins_count = np.histogram(point_dict[k], bins=10)
+    count, bins_count = np.histogram(point_dict[k], bins=25)
 
     # finding the PDF of the histogram using count values
     pdf = count / sum(count)
@@ -40,12 +65,12 @@ for k in point_dict.keys():
     cdf = np.cumsum(pdf)
 
     # plotting PDF and CDF
-    plt.plot(bins_count[1:], pdf, color="red", label="PDF MMI %i" % k)
-    plt.plot(bins_count[1:], cdf, label="CDF MMI %i" % k)
+    # plt.plot(bins_count[1:], pdf*100, color="red", label="PDF")
+    plt.plot(bins_count[1:], cdf[::-1]*100, label="MMI {} to {}".format(k, k + 0.9))
     plt.xlabel('Warning Time (s)')
-    plt.title('MMI %i' % k)
+    plt.ylabel('% of sites')
+    plt.title('Percent of sites with at least X warning time(s) MMI {} to {}'.format(k, k + 0.9))
     plt.legend()
     plt.grid()
-    plt.savefig('Figures/cdfs/CDF MMI %i' % k)
-    plt.clf()
+    plt.savefig('Figures/cdfs/Minimum WT percentages (All MMI).png')
 
