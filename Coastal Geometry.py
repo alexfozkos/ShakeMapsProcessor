a4025eb3614397d7b542a78bfa896505f787d105
alexfozkos <afozkos@alaska.edu>
2024-04-15 13:40:41 -0800

    Coastal geometry ready to run

--- Coastal Geometry.py
+++ Coastal Geometry.py
@@ -2,15 +2,12 @@
 # Its also to double check and make sure all my coastal scenarios are using the current warning time model numbers
 # and manual ShakeMap config settings.
 import os
-import json
 import matplotlib as mpl
 mpl.rcParams["backend"] = "TkAgg"
 import pandas as pd
 import numpy as np
-from numpy import sin, cos, pi
-from matplotlib import pyplot as plt
 import UsefulFunctions as uf
-
+import pygmt
 
 def fixlons(df):
     df['lon'][df['lon'] > 180] -= 360  # convert lons past 180 to negative western lons
@@ -190,3 +187,99 @@ for index, row in qcf_hypocenters.iterrows():
     ccf = LB13''')
 #endregion
 
+#simple map test, not to be used as map figure generator
+title = r"Coastal Test"
+coast_border = "a/0.25p,black"
+shorelines = "0.15p,black"
+fig = pygmt.Figure()
+fig.basemap(region=f'-170/47/-125/65+r', projection='M15c',
+            frame=["af", f'WSne+t"{title}"'])
+fig.coast(shorelines=shorelines, borders=coast_border, water='lightsteelblue1',
+          land='gainsboro')  # draw coast over datawater='skyblue'
+starsize = 0.5
+numsize = 0.2
+star_index = 1
+
+for index, row in alu_hypocenters.iterrows():
+    p, LW = uf.createPlane2(row['lon'], row['lat'], 8.3, row['depth'], row['strike'], row['dip'], 'int')
+    fig.plot(
+        x=[p[1][0], p[3][0], p[5][0], p[7][0], p[1][0]],
+        y=[p[1][1], p[3][1], p[5][1], p[7][1], p[1][1]],
+        color='red',
+        transparency='75',
+        pen='1p,gray'
+    )
+
+for index, row in alu_hypocenters.iterrows():
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'a{starsize}c',
+        color='white',
+        pen='0.25p,red'
+    )
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'l{numsize}c+t"{star_index}"',
+        color='black'
+    )
+    star_index += 1
+
+planes = {}
+for index, row in cse_hypocenters.iterrows():
+    p, LW = uf.createPlane2(row['lon'], row['lat'], 8.3, row['depth'], row['strike'], row['dip'], 'r')
+    planes[index] = p
+    fig.plot(
+        x=[p[1][0], p[3][0], p[5][0], p[7][0], p[1][0]],
+        y=[p[1][1], p[3][1], p[5][1], p[7][1], p[1][1]],
+        color='purple',
+        transparency='75',
+        pen='1p,gray'
+    )
+
+for index, row in cse_hypocenters.iterrows():
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'a{starsize}c',
+        color='white',
+        pen='0.25p,purple'
+    )
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'l{numsize}c+t"{star_index+1}"',
+        color='black'
+    )
+    star_index += 1
+
+planes = {}
+for index, row in qcf_hypocenters.iterrows():
+    p, LW = uf.createPlane2(row['lon'], row['lat'], 8.3, row['depth'], row['strike'], row['dip'], 'ss')
+    planes[index] = p
+    fig.plot(
+        x=[p[1][0], p[3][0], p[5][0], p[7][0], p[1][0]],
+        y=[p[1][1], p[3][1], p[5][1], p[7][1], p[1][1]],
+        color='green3',
+        transparency='25',
+        pen='1p,green3'
+    )
+
+for index, row in qcf_hypocenters.iterrows():
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'a{starsize}c',
+        color='white',
+        pen='0.25p,green3'
+    )
+    fig.plot(
+        x=row['lon'],
+        y=row['lat'],
+        style=f'l{numsize}c+t"{star_index}"',
+        color='black'
+    )
+    star_index += 1
+
+fig.savefig('Figures/CoastalScenarios/cp2maptest.png')
